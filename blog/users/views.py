c44111a941077930ad5fea026ef378898621d17d
Obuya-Nyang <obuyanyawanga@gmail.com>
2021-11-01 04:54:06 +0300

    Upload initial blog project files

---
+++ blog/users/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+
+from .forms import RegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect
+    else:
+        form = RegistrationForm()
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
